dbc03f5595fc1f7605b0959026100ba2d8c2fcc3
liwei1297 <j.long@outlook.sg>
2022-08-10 16:47:50 +0530

    code cleanup

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,44 +1,35 @@
-import { createContext, useEffect, useState, useContext } from "react"
-import ToastContext from "./ToastContext"
+import { createContext, useEffect, useState, useContext } from "react";
+import ToastContext from "./ToastContext";
 
 type User = {
-	account: string
-	avatar_url: string
-	profile_link: string
-	username: string
-}
+  account: string;
+  avatar_url: string;
+  profile_link: string;
+  username: string;
+};
 
 type AuthContextType = {
-	isAuthenticated: boolean
-	user: User
-}
+  isAuthenticated: boolean;
+  user: User;
+};
 
-const AuthContext = createContext({} as AuthContextType)
+const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: any) {
-	const { showSuccess, showError, showLoading } = useContext(ToastContext)
+  const { showSuccess, showError, showLoading } = useContext(ToastContext);
 
-	const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>(null);
 
-	const isAuthenticated = !!user
+  const isAuthenticated = !!user;
 
-	useEffect(() => {
-		// recoverUserInfo()
-		// 	.then((userData: User) => {
-		// 		console.log("Auth user: " + JSON.stringify(userData))
-		// 		setUser(userData)
-		// 	})
-		// 	.catch((err) => {
-		// 		console.error(err)
-		// 	})
-	}, [])
+  console.log("Code Cleanup");
+  useEffect(() => {}, []);
 
-	return (
-		<AuthContext.Provider
-			value={{ user, isAuthenticated }}>
-			{children}
-		</AuthContext.Provider>
-	)
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export default AuthContext
+export default AuthContext;
